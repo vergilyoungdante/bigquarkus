0ff80f886a914606b490662d6c6984821ee69edf
vergil <yjb387759719@outlook.com>
2022-07-20 20:24:17 +0800

    cloudProcess增删改查写完了

--- src/main/java/com/example/persistence/CloudProcessRepository.java
+++ src/main/java/com/example/persistence/CloudProcessRepository.java
@@ -1,9 +1,14 @@
 package com.example.persistence;
 
+import com.example.common.page.MyPage;
+import com.example.domain.CloudComputer;
 import com.example.domain.CloudProcess;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Page;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @BelongsProject: bigquarkus
@@ -14,4 +19,33 @@ import javax.enterprise.context.ApplicationScoped;
  */
 @ApplicationScoped
 public class CloudProcessRepository implements PanacheRepository<CloudProcess> {
+
+    public MyPage<CloudProcess> findPage(Map<String, Object> parameters, Page page){
+        MyPage<CloudProcess> result = new MyPage<>(page.index,page.size,0,null);
+        if ( parameters == null ) {
+            int total = listAll().size();
+            result.setTotal(total);
+            result.setList(findAll().page(page).list());
+            return result;
+        }
+
+        Map<String, Object> nonNullParams = parameters.entrySet().stream()
+                .filter( entry -> entry.getValue() != null )
+                .collect( Collectors.toMap( Map.Entry::getKey, Map.Entry::getValue ) );
+
+        if ( nonNullParams.isEmpty() ) {
+            int total = listAll().size();
+            result.setTotal(total);
+            result.setList(findAll().page(page).list());
+            return result;
+        }
+
+        String query = nonNullParams.entrySet().stream()
+                .map( entry -> entry.getKey() + "=:" + entry.getKey() )
+                .collect( Collectors.joining(" and ") );
+
+        result.setTotal((int)find(query, nonNullParams).count());
+        result.setList(find(query, nonNullParams).page(page).list());
+        return result;
+    }
 }
---
+++ src/main/java/com/example/rest/CloudProcessResource.java
@@ -0,0 +1,102 @@
+package com.example.rest;
+
+import com.example.common.page.MyPage;
+import com.example.common.response.ResponseUtility;
+import com.example.domain.CloudProcess;
+import com.example.persistence.CloudProcessRepository;
+import io.quarkus.panache.common.Page;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @BelongsProject: bigquarkus
+ * @BelongsPackage: com.example.rest
+ * @Author: vergil young
+ * @CreateTime: 2022-07-20  20:19
+ * @Description: TODO
+ */
+@Path("process")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
+public class CloudProcessResource {
+
+    @Inject
+    private CloudProcessRepository cloudProcessRepository;
+
+    @GET
+    @Path("count")
+    @Transactional
+    public Long count() {
+        return cloudProcessRepository.count();
+    }
+
+    @PUT
+    @Transactional
+    public Response save(CloudProcess cloudProcess) {
+        if (cloudProcess == null || cloudProcess.getId() != null) {
+            throw new WebApplicationException("id在请求中设置错误", 422);
+        }
+        cloudProcessRepository.persist(cloudProcess);
+        return ResponseUtility.createdOK(cloudProcess);
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id) {
+        if (id == null) {
+            throw new WebApplicationException("id为空", 422);
+        }
+        Boolean flag = cloudProcessRepository.deleteById(id);
+
+        return ResponseUtility.deleted(flag);
+    }
+
+    @POST
+    @Transactional
+    public Response change(CloudProcess cloudProcess) {
+        if (cloudProcess == null || cloudProcess.getId() == null) {
+            throw new WebApplicationException("id为空", 422);
+        }
+        CloudProcess change = cloudProcessRepository.findById(cloudProcess.getId());
+        change.setRequiredCpuPower(cloudProcess.getRequiredCpuPower());
+        change.setRequiredMemory(cloudProcess.getRequiredMemory());
+        change.setName(cloudProcess.getName());
+        change.setRequiredNetworkBandwidth(cloudProcess.getRequiredNetworkBandwidth());
+
+        return ResponseUtility.getOK(change);
+    }
+
+    @GET
+    @Path("{id}")
+    @Transactional
+    public Response getSingle(@PathParam("id") Long id) {
+        Optional<CloudProcess> optional = cloudProcessRepository.findByIdOptional(id);
+        if (optional.isPresent()) {
+            return ResponseUtility.getOK(optional.get());
+        }
+
+        throw new WebApplicationException("没有这个应用", 422);
+    }
+
+    @POST
+    @Path("page")
+    @Transactional
+    public Response getPage(Map<String, Object> parameters){
+        Page page = new Page((int)parameters.get("index"),(int)parameters.get("size"));
+
+        parameters.remove("index");
+        parameters.remove("size");
+
+        MyPage<CloudProcess> result = cloudProcessRepository.findPage(parameters,page);
+
+        return ResponseUtility.getOK(result);
+    }
+}
