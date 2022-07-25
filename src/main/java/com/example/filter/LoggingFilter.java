686dfe2864b7037b25d0aa76544c254eb346af04
vergil <yjb387759719@outlook.com>
2022-07-25 20:22:27 +0800

    log打印，无法打印post body参数

---
+++ src/main/java/com/example/filter/LoggingFilter.java
@@ -0,0 +1,28 @@
+package com.example.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+/**
+ * @BelongsProject: bigquarkus
+ * @BelongsPackage: com.example.filter
+ * @Author: vergil young
+ * @CreateTime: 2022-07-25  20:04
+ * @Description: TODO
+ */
+@Provider
+public class LoggingFilter implements ContainerRequestFilter {
+    Logger requestLogger = LoggerFactory.getLogger("request");
+    @Override
+    public void filter(ContainerRequestContext crc) throws IOException {
+        requestLogger.info(crc.getMethod() + " " + crc.getUriInfo().getAbsolutePath());
+        for (String key : crc.getHeaders().keySet()) {
+            requestLogger.info("[REST Logging] " +key + ": " + crc.getHeaders().get(key));
+        }
+    }
+}
---
+++ src/main/java/com/example/filter/RequestFilter.java
@@ -0,0 +1,56 @@
+package com.example.filter;
+
+import io.quarkus.vertx.http.runtime.filters.Filters;
+import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpServerRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @BelongsProject: bigquarkus
+ * @BelongsPackage: com.example.filter
+ * @Author: vergil young
+ * @CreateTime: 2022-07-25  14:44
+ * @Description: 自定义filter实现请求log打印，功能不如quarkus自带配置好用
+ */
+@ApplicationScoped
+public class RequestFilter {
+
+    Logger requestLogger = LoggerFactory.getLogger(RequestFilter.class);
+
+    public void filters(@Observes Filters filters) {
+        filters.register(
+                routingContext -> {
+                    /**filter自定义**/
+//                    HttpServerRequest request = routingContext.request();
+//                    requestLogger.info("REQUEST: url=>{},httpMethod=>{},ip=>{},httpParams=>{}",
+//                            request.uri(), request.method(), request.remoteAddress(), buildHttpParams(request));
+                    //没这句话会卡死整个filter
+                    routingContext.next();
+                },10
+        );
+    }
+
+    private String buildHttpParams(HttpServerRequest req){
+        StringBuffer paramBuf = new StringBuffer();
+        paramBuf.append("{");
+        MultiMap params = req.params();
+        for(String name:params.names()){
+            List<String> list = params.getAll(name);
+            paramBuf.append(name).append(":");
+            if(list.size()==1){
+                paramBuf.append(list.size());
+            }else {
+                list.forEach(paramBuf::append);
+            }
+        }
+        paramBuf.append("}");
+        return new String(paramBuf);
+    }
+}
